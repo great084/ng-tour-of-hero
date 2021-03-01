ccce95351e9227c639c1363f447d264fa9531646
great084 <great084@gmail.com>
2021-03-01 17:36:54 +0900

    データの削除

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -50,6 +50,17 @@ export class HeroService {
     );
   }
 
+  /** DELETE: サーバーからヒーローを削除 */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id = ${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
